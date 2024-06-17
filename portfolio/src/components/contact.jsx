44c347d4200f4d0e2578389e65fb57808f164626
Pakshal Shah <pakshalshah1927@gmail.com>
2024-06-17 17:47:34 +0530

    changes to old repo

--- portfolio/src/components/contact.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Contactform = () => {
-  return (
-    <div>Contactform</div>
-  )
-}
-
-export default Contactform
--- portfolio/src/pages/Error.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const NoPage = () => {
-  return (
-    <div>Error 404 ! Not Found !</div>
-  )
-}
-
-export default NoPage
