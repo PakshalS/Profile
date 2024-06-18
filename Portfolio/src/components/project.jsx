995d0a38ae165ff49e3a2d19506b54c888abcfe2
Pakshal Shah <pakshalshah1927@gmail.com>
2024-06-18 17:43:54 +0530

    project section init

--- Portfolio/src/components/project.jsx
+++ Portfolio/src/components/project.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div id='projects'>Projects</div>
+    <div id='projects' className='h-screen bg-black flex-row'>
+      <h1 className='text-white font-bold text-4xl flex justify-center pt-16'>
+      Projects
+      </h1>
+      <h1 className='text-white flex justify-center p-8'>
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam iure fugiat sint. Ullam, perspiciatis reprehenderit, sapiente quasi nesciunt, laudantium architecto magnam commodi iure quia vero quis sequi dolor doloremque dolores.
+      </h1>
+      <div className='grid'>
+        </div>
+        
+        </div>
   )
 }
 
