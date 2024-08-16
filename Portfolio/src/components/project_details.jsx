d16df6bc3da09ba916ae9327b754fc8ca4181789
Pakshal Shah <pakshalshah1927@gmail.com>
2024-08-16 20:03:39 +0530

    Changes

---
+++ Portfolio/src/components/project_details.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Navigationbar from './navbar'
+
+const project_details = () => {
+  return (
+    <div className='min-h-screen bg-about-back'> 
+    <Navigationbar/>
+    </div>
+  )
+}
+
+export default project_details
